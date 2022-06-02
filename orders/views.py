0c460ac4e653a8f87eda9f203e2fb87c7df5190a
pochii <johnmwanikig2000@gmail.com>
2022-06-02 20:29:41 +0300

    fifth commit

--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,6 @@
+import requests
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.views import View
@@ -14,3 +15,9 @@ class MyOrders(LoginRequiredMixin, View):
     def get(self, request):
         orders = Order.objects.all().filter(user=request.user)
         return render(request, 'my_orders.html', {'orders': orders})
+
+    def post(self, request):
+        order = Order.objects.get(id=request.POST['order_id'])
+        res = requests.post('http://localhost:8000/mpesa/submit/',
+                            data={'phone_number': order.mobile, 'amount': order.total})
+        return redirect('BaseView')
